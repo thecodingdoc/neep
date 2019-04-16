5b0df8bd6bef67bb7e32de2e4f092f7c4560c6aa
Sean West <seanchristianwest@gmail.com>
2019-04-16 09:21:07 -0500

    Uniform seed
    The uniform seed generator is now initiated regardless of if isUniform flag is active, since the c++ compiler expects it to exist.

--- neep.C
+++ neep.C
@@ -211,11 +211,9 @@ void calculateNull(vector<ClinicalSample> &clinical,
 				   double expressionThreshold,
 				   bool isUniform)
 {
-  if (isUniform){
-    unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
-    std::default_random_engine generator(seed);
-    std::uniform_real_distribution<double> uniform(0.0, 1.0);
-  }
+  unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+  std::default_random_engine generator(seed);
+  std::uniform_real_distribution<double> uniform(0.0, 1.0);
 
   // calculate the null distribution for the minimum p-value stat
   unsigned int numSamples = clinical.size();
